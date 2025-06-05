c877689df062aad022d53a9bdf545f503f60db7a
codeWithGodstime <specialgodstym@gmail.com>
2025-06-05 21:57:06 +0100

    remove pages app

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -1,13 +1,9 @@
 from pathlib import Path
 import environ
 
-# Initialize environ
 env = environ.Env()
-
-# Set the project base directory
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Take environment variables from .env file
 environ.Env.read_env(BASE_DIR / '.env')
 
 INSTALLED_APPS = [
